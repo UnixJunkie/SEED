0abdbe1cb949c7417b85eaa5080fa674df48f84e
clangi <cassiano.langini@gmail.com>
2019-11-18 10:28:52 +0100

    compressed to coordinate sparse matrix

--- src/nrutil_sparse.h
+++ src/nrutil_sparse.h
@@ -5,7 +5,8 @@
 #include <boost/numeric/ublas/io.hpp>
 
 template <class T>
-using sparse_3D = boost::numeric::ublas::compressed_matrix<T>; // type alias (C++11)
+// using sparse_3D = boost::numeric::ublas::compressed_matrix<T>; // type alias (C++11)
+using sparse_3D = boost::numeric::ublas::coordinate_matrix<T>; // type alias (C++11)
 
 typedef boost::numeric::ublas::compressed_matrix<char>::iterator1 it1_t;
 typedef boost::numeric::ublas::compressed_matrix<char>::iterator2 it2_t;
